31c0b887f009fcb388e39bbf5131fedb509c89a3
sergejpopov <spopov@velitech.com>
2024-08-28 19:04:47 +0300

    chore: two sum

---
+++ 7.two-sum/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums = []int{2, 7, 11, 15}
+
+	fmt.Println(twoSum(nums, 9))
+
+}
+
+func twoSum(nums []int, target int) []int {
+	numsMap := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		currentNum := nums[i]
+		prevNum := target - currentNum
+
+		if _, ok := numsMap[prevNum]; ok {
+			return []int{numsMap[prevNum], i}
+		}
+
+		numsMap[currentNum] = i
+	}
+	return []int{-1, 1}
+}
