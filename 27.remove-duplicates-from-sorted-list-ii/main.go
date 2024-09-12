b01510d1f28c17c64ac5e21c072bff9984cc6929
sergejpopov <spopov@velitech.com>
2024-09-12 19:22:40 +0300

    chore: remove duplicates from sorted list 2

--- 27.remove-duplicates-from-sorted-list-ii/main.go
+++ 27.remove-duplicates-from-sorted-list-ii/main.go
@@ -1,1 +1,32 @@
-package _7_remove_duplicates_from_sorted_list_ii
+package main
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{Val: -101, Next: head}
+	slow, fast := dummy, head
+
+	for fast != nil {
+		if fast.Next != nil && fast.Val == fast.Next.Val {
+			num := fast.Val
+
+			for fast != nil && fast.Val == num {
+				fast = fast.Next
+			}
+
+			if fast == nil {
+				slow.Next = fast
+			}
+		} else {
+			slow.Next = fast
+			fast = fast.Next
+			slow = slow.Next
+		}
+	}
+
+	return dummy.Next
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
