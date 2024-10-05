7adbebe9c7aa0e85084ed6cce98bf13592f9cb13
sergejpopov <spopov@velitech.com>
2024-10-05 14:07:49 +0300

    chore: binary tree level order traversal (second solution)

--- tree/39.binary-tree-level-order-traversal/main.go
+++ tree/39.binary-tree-level-order-traversal/main.go
@@ -1,7 +1,7 @@
 package main
 
 // https://leetcode.com/problems/binary-tree-level-order-traversal/
-func levelOrder(root *TreeNode) [][]int {
+func levelOrder1(root *TreeNode) [][]int {
 	var result = [][]int{}
 
 	traversal(root, &result, 0)
@@ -22,6 +22,38 @@ func traversal(node *TreeNode, result *[][]int, i int) {
 
 	traversal(node.Left, result, i+1)
 	traversal(node.Right, result, i+1)
+}
+
+// without recursion
+func levelOrder2(root *TreeNode) (result [][]int) {
+	var queue []*TreeNode
+
+	if root != nil {
+		queue = append(queue, root)
+	}
+
+	for len(queue) > 0 {
+		var arr []int
+
+		for i := len(queue); i > 0; i-- {
+			node := queue[0]
+			queue = queue[1:]
+
+			arr = append(arr, node.Val)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		result = append(result, arr)
+	}
+
+	return
 }
 
 type TreeNode struct {
