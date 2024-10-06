971b99f45ce1a3f9ea3501534b24030a7b3340a6
sergejpopov <spopov@velitech.com>
2024-10-06 17:02:20 +0300

    chore: binary tree right side view (without recursion)

--- tree/40.binary-tree-right-side-view/main.go
+++ tree/40.binary-tree-right-side-view/main.go
@@ -1,7 +1,7 @@
 package main
 
 // https://leetcode.com/problems/binary-tree-right-side-view/
-func rightSideView(root *TreeNode) []int {
+func rightSideView1(root *TreeNode) []int {
 	var result []int
 
 	traverse(root, &result, 0)
@@ -20,6 +20,37 @@ func traverse(node *TreeNode, result *[]int, i int) {
 
 	traverse(node.Right, result, i+1)
 	traverse(node.Left, result, i+1)
+}
+
+func rightSideView2(root *TreeNode) (result []int) {
+	var stack []*TreeNode
+
+	if root == nil {
+		return
+	}
+
+	stack = append(stack, root)
+
+	for len(stack) > 0 {
+		result = append(result, stack[len(stack)-1].Val)
+
+		for n := len(stack); n > 0; n-- {
+			node := stack[0]
+
+			stack = stack[1:]
+
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+
+		}
+	}
+
+	return
 }
 
 type TreeNode struct {
