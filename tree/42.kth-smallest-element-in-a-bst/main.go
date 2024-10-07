87dd57a1c9caa0c1ddd3c2f6272d7e376534c9f8
sergejpopov <spopov@velitech.com>
2024-10-07 06:06:51 +0300

    chore: kth smallest element in a bst (without recursion)

--- tree/42.kth-smallest-element-in-a-bst/main.go
+++ tree/42.kth-smallest-element-in-a-bst/main.go
@@ -1,7 +1,7 @@
 package main
 
 // https://leetcode.com/problems/kth-smallest-element-in-a-bst/
-func kthSmallest(root *TreeNode, k int) int {
+func kthSmallest1(root *TreeNode, k int) int {
 	var result = traverse(root, []int{})
 
 	return result[k-1]
@@ -16,6 +16,31 @@ func traverse(node *TreeNode, result []int) []int {
 	result = traverse(node.Right, result)
 
 	return result
+}
+
+// without recursion
+func kthSmallest2(root *TreeNode, k int) int {
+	var stack []*TreeNode
+
+	for root != nil || len(stack) > 0 {
+		if root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		} else {
+			root = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			k--
+
+			if k == 0 {
+				return root.Val
+			}
+
+			root = root.Right
+		}
+	}
+
+	return 0
 }
 
 type TreeNode struct {
