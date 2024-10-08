30735ad63a55b142f7719419d337a9cef780ba6b
sergejpopov <spopov@velitech.com>
2024-10-08 06:46:10 +0300

    chore: maximum depth of binary tree (bfs solution)

--- tree/45.maximum-depth-of-binary-tree/main.go
+++ tree/45.maximum-depth-of-binary-tree/main.go
@@ -1,7 +1,7 @@
 package main
 
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/
-func maxDepth(root *TreeNode) int {
+func maxDepth1(root *TreeNode) int {
 	var max int
 
 	dfs(root, 0, &max)
@@ -21,6 +21,35 @@ func dfs(node *TreeNode, i int, max *int) {
 
 	dfs(node.Left, i, max)
 	dfs(node.Right, i, max)
+}
+
+func maxDepth2(root *TreeNode) (max int) {
+	var queue []*TreeNode
+
+	if root == nil {
+		return
+	}
+
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		max++
+
+		for i := len(queue); i > 0; i-- {
+			node := queue[0]
+			queue = queue[1:]
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return
 }
 
 type TreeNode struct {
