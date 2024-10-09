caa82d7c510db5f05004b836a629ebd2ec29eba7
sergejpopov <spopov@velitech.com>
2024-10-09 06:47:18 +0300

    chore: .invert-binary-tree (bfs solution)

--- tree/46.invert-binary-tree/main.go
+++ tree/46.invert-binary-tree/main.go
@@ -1,6 +1,9 @@
 package main
 
-func invertTree(root *TreeNode) *TreeNode {
+//https://leetcode.com/problems/invert-binary-tree/
+
+// dfs solution
+func invertTree1(root *TreeNode) *TreeNode {
 	treverse(root)
 
 	return root
@@ -15,6 +18,32 @@ func treverse(node *TreeNode) {
 
 	treverse(node.Left)
 	treverse(node.Right)
+}
+
+// bfs solution
+func invertTree2(root *TreeNode) *TreeNode {
+	var queue []*TreeNode
+
+	if root != nil {
+		queue = append(queue, root)
+	}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+
+		node.Left, node.Right = node.Right, node.Left
+	}
+
+	return root
 }
 
 type TreeNode struct {
