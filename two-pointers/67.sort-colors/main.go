86ba5ba5d1b5cd49cce47e7fc9a9b9a0208b1c5b
sergejpopov <spopov@velitech.com>
2024-10-25 08:38:23 +0300

    chore: sort colors

---
+++ two-pointers/67.sort-colors/main.go
@@ -0,0 +1,34 @@
+package main
+
+// https://leetcode.com/problems/sort-colors/
+func sortColors1(nums []int) {
+	var i int
+
+	for n := 0; n <= 2; n++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == n {
+				i++
+			} else if nums[j] == n {
+				nums[i], nums[j] = nums[j], nums[i]
+				i++
+			}
+		}
+	}
+}
+
+func sortColors(nums []int) {
+	start, current, end := -1, 0, len(nums)
+
+	for current < end {
+		if nums[current] == 0 {
+			start++
+			nums[start], nums[current] = nums[current], nums[start]
+			current++
+		} else if nums[current] == 2 {
+			end--
+			nums[end], nums[current] = nums[current], nums[end]
+		} else {
+			current++
+		}
+	}
+}
