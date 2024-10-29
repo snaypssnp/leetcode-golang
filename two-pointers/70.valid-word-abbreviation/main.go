d47ab9a4331d0cf03b2996cc334a68825d6beba9
sergejpopov <spopov@velitech.com>
2024-10-29 07:11:20 +0200

    chore: valid word abbreviation

--- two-pointers/70.valid-word-abbreviation/main.go
+++ two-pointers/70.valid-word-abbreviation/main.go
@@ -5,15 +5,18 @@ import (
 	"strconv"
 )
 
+// https://leetcode.com/problems/valid-word-abbreviation/
 func main() {
-	fmt.Println(validWordAbbreviation("apple", "a2e"))
+	fmt.Println(validWordAbbreviation2("apple", "a2e"))
 }
 
 func isNumber(char rune) bool {
 	return char >= '0' && char <= '9'
 }
 
-func validWordAbbreviation(word string, abbr string) bool {
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+func validWordAbbreviation1(word string, abbr string) bool {
 	var str string
 	var j int
 
@@ -48,4 +51,38 @@ func validWordAbbreviation(word string, abbr string) bool {
 	}
 
 	return j == len(word)
+}
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+func validWordAbbreviation2(word string, abbr string) bool {
+	wordIndex := 0
+	abbrIndex := 0
+
+	for abbrIndex < len(abbr) {
+		if isNumber(abbr[abbrIndex]) {
+			if abbr[abbrIndex] == '0' {
+				return false
+			}
+
+			num := 0
+
+			for abbrIndex < len(abbr) && isNumber(abbr[abbrIndex]) {
+				num = num*10 + int(abbr[abbrIndex]-'0')
+				abbrIndex++
+			}
+			wordIndex += num
+
+			continue
+		}
+
+		if wordIndex >= len(word) || word[wordIndex] != abbr[abbrIndex] {
+			return false
+		}
+
+		wordIndex++
+		abbrIndex++
+	}
+
+	return wordIndex == len(word) && abbrIndex == len(abbr)
 }
