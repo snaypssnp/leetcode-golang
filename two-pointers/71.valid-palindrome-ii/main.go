4d85826c2120d4c6ca1193dd5c392edeca72760c
sergejpopov <spopov@velitech.com>
2024-10-30 06:20:48 +0200

    chore: valid palindrome ii

---
+++ two-pointers/71.valid-palindrome-ii/main.go
@@ -0,0 +1,22 @@
+package main
+
+// https://leetcode.com/problems/valid-palindrome-ii/description/
+func validPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return isPalindrome(s[i:j]) || isPalindrome(s[i+1:j+1])
+		}
+	}
+
+	return true
+}
+
+func isPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+
+	return true
+}
