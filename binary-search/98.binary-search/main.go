d7ee4c96859311d83edb0179b4670603fe0389da
sergejpopov <spopov@velitech.com>
2024-11-25 06:31:38 +0200

    chore: binary search (recursion solution)

--- binary-search/98.binary-search/main.go
+++ binary-search/98.binary-search/main.go
@@ -1,7 +1,7 @@
 package main
 
 // https://leetcode.com/problems/binary-search/
-func search(nums []int, target int) int {
+func search1(nums []int, target int) int {
 	for left, right := 0, len(nums)-1; left <= right; {
 		mid := (left + right) / 2
 
@@ -17,4 +17,29 @@ func search(nums []int, target int) int {
 	}
 
 	return -1
+}
+
+// recursion solution
+func search2(nums []int, target int) int {
+	return binarySearch(nums, target, 0, len(nums)-1)
+}
+
+func binarySearch(nums []int, target, left, right int) int {
+	if left > right {
+		return -1
+	}
+
+	mid := left + (right-left)/2
+
+	cur := nums[mid]
+
+	if cur == target {
+		return mid
+	}
+
+	if cur < target {
+		return binarySearch(nums, target, mid+1, right)
+	}
+
+	return binarySearch(nums, target, left, mid-1)
 }
