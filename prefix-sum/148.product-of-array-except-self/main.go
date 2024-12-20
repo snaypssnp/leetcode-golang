bc8558651028d55bb5a6c72f6491fe7af44942d9
sergejpopov <spopov@velitech.com>
2024-12-20 05:48:52 +0200

    chore: product of array except self

--- prefix-sum/148.product-of-array-except-self/main.go
+++ prefix-sum/148.product-of-array-except-self/main.go
@@ -3,16 +3,16 @@ package main
 // https://leetcode.com/problems/product-of-array-except-self/
 func productExceptSelf(nums []int) []int {
 	res := make([]int, len(nums))
-	res[0] = 1
 
-	for i := 1; i < len(nums); i++ {
-		res[i] = res[i-1] * nums[i-1]
+	for i, prefix := 0, 1; i < len(nums); i++ {
+		res[i] = prefix
+		prefix *= nums[i]
 	}
 
-	for i, j := len(nums)-1, 1; i >= 0; i-- {
-		res[i] *= j
+	for i, postfix := len(nums)-1, 1; i >= 0; i-- {
+		res[i] *= postfix
 
-		j *= nums[i]
+		postfix *= nums[i]
 	}
 
 	return res
