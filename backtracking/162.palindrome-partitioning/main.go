ed874c316b101dccdfa1e8285705982548a78c30
sergejpopov <spopov@velitech.com>
2025-01-06 07:04:30 +0200

    chore: palindrome partitioning

---
+++ backtracking/162.palindrome-partitioning/main.go
@@ -0,0 +1,39 @@
+package main
+
+func partition(s string) (res [][]string) {
+	var backtracking func(curr []string, first int)
+
+	backtracking = func(curr []string, first int) {
+		if first == len(s) {
+			sub := make([]string, len(curr))
+			copy(sub, curr)
+			res = append(res, sub)
+
+			return
+		}
+
+		for i := first; i < len(s); i++ {
+			subStr := s[first : i+1]
+
+			if isPalindrome(subStr) {
+				curr = append(curr, subStr)
+				backtracking(curr, i+1)
+				curr = curr[:len(curr)-1]
+			}
+		}
+	}
+
+	backtracking([]string{}, 0)
+
+	return
+}
+
+func isPalindrome(str string) bool {
+	for left, right := 0, len(str)-1; left <= right; left, right = left+1, right-1 {
+		if str[left] != str[right] {
+			return false
+		}
+	}
+
+	return true
+}
